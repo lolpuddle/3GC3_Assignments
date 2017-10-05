f77ed8d6af3db5d3205bb576b05e57d084b512bf
lolpuddle <oocean.cheung@gmail.com>
2017-10-05 04:14:57 -0400

    so tired

--- a1/draw.cpp
+++ a1/draw.cpp
@@ -14,63 +14,110 @@ void drawPoint(int x, int y) {
     points.push_back(point);
 }
 
-void drawLineUp(int x, int y, int x2, int y2) {
-    int dx = abs(x2 - x); int dy = abs(y2 - y);
-    int d = 2*dy - dx;
-    int incrE = dy*2;
-    int incrNE = dy*2 - dx*2;
-    drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+// void drawLineUp(int x, int y, int x2, int y2) {
+//     int dx = abs(x2 - x); int dy = abs(y2 - y);
+//     int d = 2*dy - dx;
+//     int incrE = dy*2;
+//     int incrNE = dy*2 - dx*2;
+//     drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
 
-    while((x < x2) && (y < y2)) {
-        if (d > 0) {
-            d += incrE;
-            x++;
-        } else {
-            d += incrNE;
-            x++;
-            y--;
-        }
-        drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
-    }
-}
+//     while((x < x2) && (y < y2)) {
+//         if (d > 0) {
+//             d += incrE;
+//             x++;
+//         } else {
+//             d += incrNE;
+//             x++;
+//             y--;
+//         }
+//         drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+//     }
+// }
 
-void drawLineDown(int x, int y, int x2, int y2) {
-    int dx = x2 - x; int dy = y2 - y;
-    int d = 2*dy - dx;
-    int incrE = dy*2;
-    int incrSE = dy*2 - dx*2;
-    drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+// void drawLineDown(int x, int y, int x2, int y2) {
+//     int dx = x2 - x; int dy = y2 - y;
+//     int d = 2*dy - dx;
+//     int incrE = dy*2;
+//     int incrSE = dy*2 - dx*2;
+//     drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
 
-    while(x < x2) {
-        if (d <= 0) {
-            d += incrE;
-            x++;
-        } else {
-            d += incrSE;
-            x++;
-            y++;
-        }
-        drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
-    }
-}
+//     while(x < x2) {
+//         if (d <= 0) {
+//             d += incrE;
+//             x++;
+//         } else {
+//             d += incrSE;
+//             x++;
+//             y++;
+//         }
+//         drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+//     }
+// }
 
-void drawLine( int x, int y, int x2, int y2) {
-    int point1_x = x;
-    int point1_y = y;
-    int point2_x = x2;
-    int point2_y = y2;
+// void drawLine( int x, int y, int x2, int y2) {
+//     int point1_x = x;
+//     int point1_y = y;
+//     int point2_x = x2;
+//     int point2_y = y2;
     
-    if (x > x2) {
-        point1_x = x2;
-        point1_y = y2;
-        point2_x = x;
-        point2_y = y;
+//     if (x > x2) {
+//         point1_x = x2;
+//         point1_y = y2;
+//         point2_x = x;
+//         point2_y = y;
+//     }
+
+//     if (point1_y > point2_y) {
+//         drawLineUp(point1_x, point1_y, point2_x, point2_y);
+//     } else {
+//         drawLineDown(point1_x, point1_y, point2_x, point2_y);
+//     }
+// }
+
+
+// Found pseudocode explanation online in "Optimization" section
+// Of the page below:
+// http://dictionnaire.sensagent.leparisien.fr/Bresenham's%20algorithm/en-en/
+void drawLine(int x, int y, int x2, int y2) {
+    bool steepSlope = abs(y2 - y) > abs(x2 - x);
+    if (steepSlope) {
+        int temp = x;
+        x = y;
+        y = temp;
+
+        temp = x2;
+        x2 = y2;
+        y2 = temp;
     }
 
-    if (point1_y > point2_y) {
-        drawLineUp(point1_x, point1_y, point2_x, point2_y);
-    } else {
-        drawLineDown(point1_x, point1_y, point2_x, point2_y);
+    if (x > x2) {
+        int tempX = x;
+        int tempY = y;
+        x = x2;
+        y = y2;
+        x2 = tempX;
+        y2 = tempY;
+    }
+
+    int dx = x2 - x;
+    int dy = abs(y2 - y);
+    int err = 0;
+    int ystep = -1;
+    int y3 = y;
+    if (y < y2) {
+        ystep = 1;
+    } 
+    for (int x3 = x; x3 < x2; x3++) {
+        if (steepSlope) {
+            drawPoint(y3, glutGet(GLUT_WINDOW_HEIGHT) - x3);
+        } else {
+            drawPoint(x3, glutGet(GLUT_WINDOW_HEIGHT) - y3);
+        }
+        err -= dy;
+        if (err < 0) {
+            y3 += ystep;
+            err += dx;
+        }
     }
 }
 
@@ -101,7 +148,8 @@ void drawRectangle(int x, int y, int x2, int y2) {
 }
 
 
-// With help from John Kennedy:
+// With help from John Kennedy's Paper:
+// "A Fast Bresenham Type Algorithm For Drawing Circles"
 // http://web.engr.oregonstate.edu/~sllu/bcircle.pdf
 void drawCircle(int x, int y, int x2, int y2) {
     int r = (int) sqrt(pow((double) (y2 - y), 2.0) + pow((double) (x2 - x), 2.0));
@@ -110,7 +158,7 @@ void drawCircle(int x, int y, int x2, int y2) {
     int dx = 1 - 2*r;
     int dy = 1;
     int re = 0;
-    
+
     while (x3 >= y3) {
         drawPoint(x + x3, glutGet(GLUT_WINDOW_HEIGHT) - y - y3);
         drawPoint(x - x3, glutGet(GLUT_WINDOW_HEIGHT) - y - y3);
@@ -131,10 +179,16 @@ void drawCircle(int x, int y, int x2, int y2) {
     }
 }
 
+void drawRadial(int x, int y) {
+    
+}
+
 void mouse(int btn, int state, int x, int y) {
     if (brush.brushType == 1) {
         drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
-    }
+    } else if (brush.brushType == 5) (
+        drawRadial(x, y);
+    )
     if(btn == GLUT_LEFT_BUTTON) {
         if(state == GLUT_DOWN) {
             if (firstClick) {
@@ -162,5 +216,7 @@ void mouse(int btn, int state, int x, int y) {
 void mouseMotion(int x, int y) {
     if (brush.brushType == 1) {
         drawPoint(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+    } else if (brush.brushType == 5) {
+        drawRadial(x, y);
     }
 }
